a7b5b86d01a3b00d9734b375e5e452f789c093a2
Mohamed Sherif <mohamedsherif2395@gmail.com>
2024-09-10 23:11:38 +0300

    error handling for order creation and sales report generation

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,11 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateOrderDto, ItemDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Order } from './schemas/order.schema';
 import { Model } from 'mongoose';
 import * as moment from 'moment';
-import { CacheService } from '../cache/cache.service';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { CacheInvalidatedEvent } from '../../types/events';
 import { redisKeys } from '../../configs/constants';
@@ -24,92 +28,120 @@ export class OrdersService {
   async findOne(id: string) {
     return this.orderModel.findById(id);
   }
-  
+
   async create(createOrderDto: CreateOrderDto) {
-    const documentData: Order = {
-      ...createOrderDto,
-      totalPrice: createOrderDto.items.reduce((acc, curr) => acc += (curr.price * curr.quantity), 0),
+    try {
+      const documentData: Order = {
+        ...createOrderDto,
+        totalPrice: this.calculateOrderTotalPrice(createOrderDto.items),
+      };
+      const order = await this.orderModel.create(documentData);
+      this.eventEmmitter.emitAsync(
+        CacheInvalidatedEvent.eventName,
+        new CacheInvalidatedEvent(redisKeys.SALES_REPORT),
+      );
+
+      return order;
+    } catch (err) {
+      const msg = 'Something went wrong while adding the order data';
+      console.error(msg, err);
+      throw new HttpException(msg, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    const order = await this.orderModel.create(documentData);
-    this.eventEmmitter.emitAsync(CacheInvalidatedEvent.eventName, new CacheInvalidatedEvent(redisKeys.SALES_REPORT));
-    
-    return order;
   }
 
   async update(id: string, updateOrderDto: UpdateOrderDto) {
-    const updateData: Partial<Order> = {...updateOrderDto};
-    if (updateOrderDto.items?.length) updateData.totalPrice = updateOrderDto.items.reduce((acc, curr) => acc += (curr.price * curr.quantity), 0);
+    const updateData: Partial<Order> = { ...updateOrderDto };
+    if (updateOrderDto.items?.length) updateData.totalPrice = this.calculateOrderTotalPrice(updateOrderDto.items);
 
-    const updatedOrder = await this.orderModel.findByIdAndUpdate(id, updateData, { new: true });
+    const updatedOrder = await this.orderModel.findByIdAndUpdate(
+      id,
+      updateData,
+      { new: true },
+    );
 
-  if (!updatedOrder) {
-    throw new NotFoundException(`Order with ID ${id} not found`);
+    if (!updatedOrder) {
+      throw new NotFoundException(`Order with ID ${id} not found`);
+    }
+    this.eventEmmitter.emitAsync(
+      CacheInvalidatedEvent.eventName,
+      new CacheInvalidatedEvent(redisKeys.SALES_REPORT),
+    );
+
+    return updatedOrder;
   }
-  this.eventEmmitter.emitAsync(CacheInvalidatedEvent.eventName, new CacheInvalidatedEvent(redisKeys.SALES_REPORT));
 
-  return updatedOrder;
-  }
-
-  async generateDailySalesReport(date?: Date): Promise<any> {
+  async generateDailySalesReport(date?: Date) {
     const reportDate = date ? new Date(date) : new Date();
 
     const startOfDay = moment(reportDate).startOf('day').toDate();
     const endOfDay = moment(reportDate).endOf('day').toDate();
-
-    const report = await this.orderModel.aggregate([
-      {
+    try {
+      const report = await this.orderModel.aggregate([
+        {
           $match: {
-              createdAt: {
-                  $gte: startOfDay,
-                  $lt: endOfDay
-              }
-          }
-      },
-      {
+            createdAt: {
+              $gte: startOfDay,
+              $lt: endOfDay,
+            },
+          },
+        },
+        {
           $facet: {
-              salesStats: [
-                  {
-                      $group: {
-                          _id: null,
-                          totalOrders: { $sum: 1 },
-                          totalRevenue: { $sum: "$totalPrice" }
-                      }
-                  }
-              ],
-              topSellingItems: [
-                  { $unwind: "$items" },
-                  {
-                      $group: {
-                          _id: "$items.name",
-                          totalQuantity: { $sum: "$items.quantity" }
-                      }
-                  },
-                  { $sort: { totalQuantity: -1 } },
-                  { $limit: 10 },
-                  { 
-                    $project: {
-                      _id: 0,
-                      name: "$_id",
-                      totalQuantity: 1
-                    }
-                  }
-              ]
-          }
-      },
-      {
-        $project: {
-            totalOrders: { $arrayElemAt: ["$salesStats.totalOrders", 0] },
-            totalRevenue: { $arrayElemAt: ["$salesStats.totalRevenue", 0] },
-            topSellingItems: 1
-        }
-      }
-    ]);
+            salesStats: [
+              {
+                $group: {
+                  _id: null,
+                  totalOrders: { $sum: 1 },
+                  totalRevenue: { $sum: '$totalPrice' },
+                },
+              },
+            ],
+            topSellingItems: [
+              { $unwind: '$items' },
+              {
+                $group: {
+                  _id: '$items.name',
+                  totalQuantity: { $sum: '$items.quantity' },
+                },
+              },
+              { $sort: { totalQuantity: -1 } },
+              { $limit: 10 },
+              {
+                $project: {
+                  _id: 0,
+                  name: '$_id',
+                  totalQuantity: 1,
+                },
+              },
+            ],
+          },
+        },
+        {
+          $project: {
+            totalOrders: { $arrayElemAt: ['$salesStats.totalOrders', 0] },
+            totalRevenue: { $arrayElemAt: ['$salesStats.totalRevenue', 0] },
+            topSellingItems: 1,
+          },
+        },
+      ]);
 
-    return {
-      date: startOfDay.toISOString().slice(0, 10),
-      totalRevenue: report[0]?.totalRevenue || 0,
-      totalOrders: report[0]?.totalOrders || 0,
-      topSellingItems: report[0]?.topSellingItems || [],
-    };
+      return {
+        date: startOfDay.toISOString().slice(0, 10),
+        totalRevenue: report[0]?.totalRevenue || 0,
+        totalOrders: report[0]?.totalOrders || 0,
+        topSellingItems: report[0]?.topSellingItems || [],
+      };
+    } catch (err) {
+      const msg = 'Something went wrong while calculating the sales report';
+      console.error(msg, err);
+      throw new HttpException(msg, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  calculateOrderTotalPrice(items: ItemDto[]) {
+    return items.reduce(
+      (acc, curr) => (acc += curr.price * curr.quantity),
+      0,
+    )
   }
 }
